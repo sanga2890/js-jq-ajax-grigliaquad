51c829496773f4fee2a32f60cab6d6d66cbb9c78
Stefano <stefano2890@gmail.com>
2020-05-19 15:16:42 +0200

    riciclato vecchio esercizio griglia quad

---
+++ main.js
@@ -0,0 +1,4 @@
+// Create in HTML una griglia di 36 quadratini (6x6).
+// Ad ogni click su un quadratino, parte una richiesta ajax per recuperare un numero random tra 1 e 9.
+// Se il numero restituito dall'api è <= 5, il quadrato su cui l'utente ha cliccato diventa giallo; se invece il numero restituito dall'api è > 5, il quadrato su cui l'utente ha cliccato diventa verde.
+// In entrambi i casi, andiamo ad inserire nel quadrato il numero restituito dall'api.
